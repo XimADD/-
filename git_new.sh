b65bbb832308a11fe9619144f2e14b48d75624ac
XimADD <156800261+XimADD@users.noreply.github.com>
2024-01-17 01:44:47 +0800

    Update git_new.sh

--- git_new.sh
+++ git_new.sh
@@ -5,7 +5,7 @@ export PATH
 # 新的所有者和组，需要替换为实际的用户名和组名
 OWNER="root"
 GROUP="root"
-SITEDIR="/root"
+SITEDIR="/git_ew"
 
 ChangeOwnerExcludeDir() {
     local targetDir=$1
@@ -52,7 +52,7 @@ PullUI()
 
 DoCrond()
 {
-    PullUI "git_ew" "remotes/origin/main"
+    PullUI "XimADD" "origin/main"
     sleep 10
 }
 
